5bcd7e24f7b6c645d2d57c75a98500de1037b9c4
curoli <curoli@gmail.com>
2022-04-03 17:02:30 -0400

    Adding column with UBAM file name to table of (singleton) list of lists of UBAMs.

--- src/ubams.rs
+++ src/ubams.rs
@@ -30,7 +30,7 @@ pub(crate) fn create_ubams_lists(config: UbamsConfig) -> Result<(), Error> {
     let reader = BufReader::new(input_file);
     let output_file = File::create(&config.output)?;
     let mut writer = BufWriter::new(output_file);
-    writeln!(writer, "entity:sample_id\tubams_list")?;
+    writeln!(writer, "entity:sample_id\tubam\tubams_list")?;
     for line in reader.lines() {
         let line = line?;
         let ubam_file_name =
@@ -41,7 +41,7 @@ pub(crate) fn create_ubams_lists(config: UbamsConfig) -> Result<(), Error> {
         let ubam_list_file_local = format!("{}{}.ubams", config.target, sample_id);
         let ubam_list_file_remote = format!("{}{}.ubams", config.prefix, sample_id);
         write_ubams_list(&ubam_list_file_local, &line)?;
-        writeln!(writer, "{}\t{}", sample_id, ubam_list_file_remote)?;
+        writeln!(writer, "{}\t{}\t{}", sample_id, line, ubam_list_file_remote)?;
     }
     writer.flush()?;
     Ok(())
